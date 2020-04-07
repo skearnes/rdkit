ebf041773bee9009b4b00b6583eebd2ebd0910a5
Steven Kearnes <kearnes@google.com>
2020-04-07 08:56:30 -0700

    smaller

--- Code/MinimalLib/jswrapper.cpp
+++ Code/MinimalLib/jswrapper.cpp
@@ -16,17 +16,9 @@ EMSCRIPTEN_BINDINGS(RDKit_minimal) {
   class_<JSMol>("Mol")
       .function("is_valid", &JSMol::is_valid)
       .function("get_smiles", &JSMol::get_smiles)
-      .function("get_molblock", &JSMol::get_molblock)
       .function("get_inchi", &JSMol::get_inchi)
       .function("get_substruct_match", &JSMol::get_substruct_match)
       .function("get_substruct_matches", &JSMol::get_substruct_matches)
-
-      // functionality primarily useful in ketcher
-      .function("get_stereo_tags", &JSMol::get_stereo_tags)
-      .function("get_aromatic_form", &JSMol::get_aromatic_form)
-      .function("get_kekule_form", &JSMol::get_kekule_form)
-      .function("add_hs", &JSMol::add_hs)
-      .function("remove_hs", &JSMol::remove_hs);
 
   function("version", &version);
   function("get_inchikey_for_inchi", &get_inchikey_for_inchi);
